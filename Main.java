9422b4a124b059986d43f87819313e4037b5937f
MegaDen58 <e@w.com>
2022-04-09 11:04:04 +0300

    added

--- Main.java
+++ Main.java
@@ -1,33 +1,19 @@
 package com.company;
+import java.util.regex.*;
 import java.util.Scanner;
 
 
-class XYZ{
-    double x;
-    double y;
 
-    void Sum(double x, double y){
-        double z;
-        if(y + 1 != 0){
-            z = (x + y) / (y + 1);
-            System.out.printf("Ответ: %2f", z);
-
-        }
-        else{
-            System.out.println("Делитель равен 0");
-        }
-    }
-}
 public class Main {
-
-    public static void main(String[] args)
-    {
-	Scanner in = new Scanner(System.in);
-    XYZ xyz = new XYZ();
-    System.out.print("Введите x: ");
-    double x = in.nextDouble();
-    System.out.print("Введите y: ");
-    double y = in.nextDouble();
-    xyz.Sum(x,y);
+    public static void main(String[] args) {
+        String regexp1 = "((((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\.){3}(((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))";
+        Pattern pattern = Pattern.compile(regexp1);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите предложение: ");
+        String word = in.nextLine();
+        Matcher m = pattern.matcher(word);
+        if(m.find()){
+            System.out.printf("Корректный IP адрес: %s.", m.group());
+        }
     }
 }

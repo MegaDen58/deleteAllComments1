5641d4934daa4b8ce3dcc2a18dec4a8d4b0da0be
MegaDen58 <e@w.com>
2022-04-15 12:09:43 +0300

    added

--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
 package com.company;
+import java.io.*;
 import java.util.regex.*;
-import java.util.Scanner;
-
-
 
 public class Main {
-    public static void main(String[] args) {
-        String regexp1 = "((((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\.){3}(((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))";
-        Pattern pattern = Pattern.compile(regexp1);
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите предложение: ");
-        String word = in.nextLine();
-        Matcher m = pattern.matcher(word);
-        if(m.find()){
-            System.out.printf("Корректный IP адрес: %s.", m.group());
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("input.txt");
+        FileWriter fileWriter = new FileWriter("output.txt");
+        int k = 0;
+        String text = "";
+        while((k=fileReader.read()) != -1){
+            text += (char)k;
         }
+
+        String a = text.replaceAll("\\//.+", "");
+        String b = a.replaceAll("/\\*(\r\n.+)*\\*/", "");
+        String c = b.replaceAll("\\/\\*([\\S\\s]+)\\*\\/", "");
+        fileWriter.write(c);
+        fileWriter.flush();
     }
 }

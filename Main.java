bc47196086ccd52f4fa585e8d51e206bdef9c42a
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-04-22 17:47:14 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,26 @@
 package com.company;
 import java.io.*;
-import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FileReader fileReader = new FileReader("input.txt");
-        FileWriter fileWriter = new FileWriter("output.txt");
-        int k = 0;
-        String text = "";
-        while((k=fileReader.read()) != -1){
-            text += (char)k;
-        }
+        try{
+            FileReader fileReader = new FileReader("input.txt");
+            int k = 0;
+            String text = "";
+            while((k = fileReader.read()) != -1){
+                text += (char)k;
+            }
 
-        String a = text.replaceAll("\\//.+", "");
-        String b = a.replaceAll("/\\*(\r\n.+)*\\*/", "");
-        String c = b.replaceAll("\\/\\*([\\S\\s]+)\\*\\/", "");
-        fileWriter.write(c);
-        fileWriter.flush();
+            String a = text.replaceAll("\\//.+", "");
+            String b = a.replaceAll("/\\*(\r\n.+)*\\*/", "");
+            String c = b.replaceAll("\\/\\*([\\S\\s]+)\\*\\/", "");
+
+            FileWriter fileWriter = new FileWriter("output.txt");
+            fileWriter.write(c);
+            fileWriter.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
